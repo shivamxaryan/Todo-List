b5ecc170d7c2ceec3e8d28d618c83dd1055306fe
Shivam <105743328+shivamxaryan@users.noreply.github.com>
2023-07-22 12:24:47 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose =require('mongoose');
 
-mongoose.connect('mongodb://0.0.0.0/todolist_db');
+mongoose.connect('mongodb+srv://shivaminsta64:mongo1234@cluster0.vwnc4jq.mongodb.net/');
 const db=mongoose.connection;
 
 db.on('error',console.error.bind(console,'error connecting to mongo db'));
